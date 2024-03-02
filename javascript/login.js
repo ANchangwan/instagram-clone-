a6d6ef804718884c83af429fc09c5dbbd4700d69
안창완 <waney@anchang-wan-ui-MacBookPro.local>
2024-03-03 00:51:51 +0900

    간단한 로그인 인증

--- javascript/login.js
+++ javascript/login.js
@@ -1,11 +1,20 @@
+const login_box = document.getElementById("login-box");
+const email = login_box.querySelector("#em");
+const password = login_box.querySelector("#password");
+
 function validateForm(e) {
   e.preventDefault();
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  console.log(email, password);
-  if (email === "ssdii66@gmail.com" && password === 111) {
+  console.log(email.value === "ssdii66@gmail.com");
+
+  if (email.value === "ssdii66@gmail.com" && password.value === "111") {
     alert("확인되었습니다.");
-    window.location.href = "main.html";
+    location.replace("main.html");
+  } else if (password.value != 111) {
+    alert("비밀번호가 틀렸습니다.");
+    location.replace("login.html");
+  } else if (email.value !== "ssdii66@gmail.com") {
+    alert("메일이 틀렸습니다!");
+    location.replace("login.html");
   }
 }
 
