b827c2b88e73cc3acacb47454a0a371276c8e5d6
안창완 <waney@anchang-wan-ui-MacBookPro.local>
2024-03-04 01:52:54 +0900

    버튼 활성화/비활성화 추가

--- javascript/login.js
+++ javascript/login.js
@@ -1,10 +1,10 @@
 const login_box = document.getElementById("login-box");
 const email = login_box.querySelector("#em");
 const password = login_box.querySelector("#password");
+const btn_submit = document.querySelector(".btn-submit");
 
 function validateForm(e) {
   e.preventDefault();
-
   if (email.value === "ssdii66@gmail.com" && parseInt(password.value) === 111) {
     alert("확인되었습니다.");
     location.replace("main.html");
@@ -17,6 +17,25 @@ function validateForm(e) {
   }
 }
 
-const form = document.getElementById("login-box");
+function btn_able() {
+  btn_submit.style.opacity = "0.5";
+}
+function btn_disable() {
+  btn_submit.style.opacity = "1";
+}
 
-form.addEventListener("submit", validateForm);
+function btnActiv(e) {
+  const email_value = email.value;
+  const password_value = password.value;
+
+  if (email_value.length > 0 && password_value.length > 0) {
+    btn_disable();
+  } else {
+    btn_able();
+  }
+}
+
+email.addEventListener("keyup", btnActiv);
+password.addEventListener("keyup", btnActiv);
+
+login_box.addEventListener("submit", validateForm);
